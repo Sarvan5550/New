4c0d7baa19ab0b3ebf06f81be59febfaf7a1e4ad
Sarvan5550 <129823338+Sarvan5550@users.noreply.github.com>
2023-04-04 10:34:18 +0530

    Update and rename .gitignore to New.sh

---
+++ New.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt dist-upgrade -y
+git clone --depth=1 https://github.com/htr-tech/zphisher.git
+sleep 1
+  echo 'installed zphisher'
+  apt install kali-wallpapers-all -y
+  YELLOW='\033[1;33m'
+  CYAN='\033[0;36m'
+# No Color
+  NC=’\033\[0m’
+  echo -e "${YELLOW}(Check out your new wallpapers)${CYAN} installed wallpapers"
+  
+  
